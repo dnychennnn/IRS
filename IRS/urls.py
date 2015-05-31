008fb0d97b028c50ec8be0ed9139c89d261c547c
Denny <denny1232@gmail.com>
2015-05-31 21:37:07 +0800

    finish our database.

--- IRS/urls.py
+++ IRS/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^guest/', include('guest.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
 )
--- guest/migrations/0001_initial.py
+++ guest/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Agent',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=200)),
                 ('is_banned', models.BooleanField(default=True)),
             ],
@@ -24,10 +24,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('Item_text', models.CharField(max_length=200)),
-                ('interval', models.CharField(max_length=200)),
-                ('pub_date', models.DateTimeField(verbose_name='date published')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('number', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ItemType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('Item_text', models.CharField(max_length=20)),
             ],
             options={
             },
@@ -36,15 +44,32 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Record',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('start', models.DateTimeField(verbose_name='start_time')),
-                ('end', models.DateTimeField(verbose_name='end_time')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('department', models.CharField(max_length=2)),
+                ('grade', models.CharField(max_length=1)),
+                ('phone', models.CharField(max_length=15)),
+                ('start_date', models.DateTimeField(verbose_name='start_time')),
+                ('end_date', models.DateTimeField(null=True, verbose_name='end_time')),
+                ('number_of_days', models.IntegerField()),
+                ('identification', models.CharField(max_length=5)),
+                ('person_in_charge', models.CharField(max_length=4)),
                 ('is_deleted', models.BooleanField(default=True)),
                 ('agent', models.ForeignKey(to='guest.Agent')),
-                ('item', models.ForeignKey(to='guest.Item')),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='ItemType',
+            field=models.ForeignKey(to='guest.ItemType'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='Record',
+            field=models.ForeignKey(to='guest.Record'),
+            preserve_default=True,
         ),
     ]
